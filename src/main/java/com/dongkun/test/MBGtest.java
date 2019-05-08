104a2be043a90e2677881747cddfdb71b6d8bb8b
vicentdk77 <vicentdk77@users.noreply.github.com>
2019-05-08 08:03:12 +0800

    333

--- src/main/java/com/dongkun/test/MBGtest.java
+++ src/main/java/com/dongkun/test/MBGtest.java
@@ -1,9 +1,29 @@
 package com.dongkun.test;
 
+import java.io.File;
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.ibatis.session.Configuration;
+import org.mybatis.generator.api.MyBatisGenerator;
+import org.mybatis.generator.config.xml.ConfigurationParser;
+import org.mybatis.generator.exception.InvalidConfigurationException;
+import org.mybatis.generator.exception.XMLParserException;
+import org.mybatis.generator.internal.DefaultShellCallback;
+
 public class MBGtest {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws InvalidConfigurationException, IOException, XMLParserException, SQLException, InterruptedException {
+		List<String> warnings = new ArrayList<String>();
+		   boolean overwrite = true;
+		   File configFile = new File("MBG.xml");
+		   ConfigurationParser cp = new ConfigurationParser(warnings);
+		   org.mybatis.generator.config.Configuration config = cp.parseConfiguration(configFile);
+		   DefaultShellCallback callback = new DefaultShellCallback(overwrite);
+		   MyBatisGenerator myBatisGenerator = new MyBatisGenerator(config, callback, warnings);
+		   myBatisGenerator.generate(null);
 
 	}
 
